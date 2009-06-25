ccfd9463ffd60713394e49ab8952da737c2edb73
tomasorti <tomasorti@6f4e8a0e-61d6-11de-85c0-7b9747e48d0e>
2009-06-25 22:58:49 +0000

    xxx

---
+++ dnsServer/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * File:   main.cpp
+ * Author: torti
+ *
+ * Created on 22 de junio de 2009, 15:37
+ */
+
+#include <iostream>
+#include <sys/socket.h>
+using namespace std;
+/*
+ *
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello Cpp!" << endl;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    cout << "sockfd=" << sockfd << endl;
+    return (EXIT_SUCCESS);
+}
